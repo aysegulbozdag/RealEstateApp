62a2dbdeb16c0f0d0bc9dab497f4e9e1d36fdeee
Ayşegül Bozdağ <aysegulbozdag4@gmail.com>
2024-03-20 19:18:22 +0300

    Detail UI completed.

--- app/src/main/java/com/example/realestateapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/realestateapp/ui/MainActivity.kt
@@ -5,18 +5,17 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
 import com.example.realestateapp.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-       /* window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-        window.statusBarColor = Color.TRANSPARENT*/
     }
 
 }
--- app/src/main/java/com/example/realestateapp/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/realestateapp/ui/detail/DetailFragment.kt
@@ -4,32 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.realestateapp.R
+import com.example.realestateapp.databinding.FragmentDetailBinding
+import com.example.realestateapp.ui.main.MainViewModel
+import com.example.realestateapp.util.base.BaseFragment
 
-class DetailFragment : Fragment() {
+class DetailFragment : BaseFragment<FragmentDetailBinding, DetailViewModel>() {
 
-    companion object {
-        fun newInstance() = DetailFragment()
-    }
+    override val viewModel: DetailViewModel by viewModels()
+    override fun getLayoutId(): Int = R.layout.fragment_detail
 
-    private lateinit var viewModel: DetailViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        (requireActivity() as? AppCompatActivity)?.supportActionBar?.apply {
-            // Geri tuşunu göster
-            setDisplayHomeAsUpEnabled(true)
-            // Anasayfa simgesini göster
-           // setDisplayShowHomeEnabled(true)
-        }
-        return inflater.inflate(R.layout.fragment_detail, container, false)
-    }
-
-
+    override fun onFragmentStarted() {
+        requireActivity().enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(getDataBinding().containerll) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }    }
 
 }
